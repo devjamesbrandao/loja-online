1d10eb56b91f258b7b21e08c2183fbab0746d9dd
James Brandao <muricijames@gmail.com>
2022-01-12 22:35:30 -0300

    Interface do repositório genérico

---
+++ Loja.Core/Data/IRepository.cs
@@ -0,0 +1,9 @@
+using Loja.Core.Interfaces;
+
+namespace Loja.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
