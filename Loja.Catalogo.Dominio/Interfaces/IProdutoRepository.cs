1dd3c27491d880b5696e363660e5caa7e5745312
James Brandao <muricijames@gmail.com>
2022-01-12 22:35:09 -0300

    Interface do repositório de produtos

---
+++ Loja.Catalogo.Dominio/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,19 @@
+using Loja.Catalogo.Dominio.Entidades;
+using Loja.Core.Data;
+
+namespace Loja.Catalogo.Dominio.Interfaces
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterTodos();
+        Task<Produto> ObterPorId(Guid id);
+        Task<IEnumerable<Produto>> ObterPorCategoria(int codigo);
+        Task<IEnumerable<Categoria>> ObterCategorias();
+
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto);
+
+        void Adicionar(Categoria categoria);
+        void Atualizar(Categoria categoria);
+    }
+}
