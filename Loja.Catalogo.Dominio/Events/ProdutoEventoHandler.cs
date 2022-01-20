562bd8ba82865f46c9c98949ac539d43a6c1fa06
James Brandao <muricijames@gmail.com>
2022-01-19 22:29:44 -0300

    Gerencia eventos de produtos

---
+++ Loja.Catalogo.Dominio/Events/ProdutoEventoHandler.cs
@@ -0,0 +1,20 @@
+using Loja.Catalogo.Dominio.Interfaces;
+using MediatR;
+
+namespace Loja.Catalogo.Dominio.Events
+{
+    public class ProdutoEventoHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventoHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent mensagem, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(mensagem.IdAgregado);
+        }
+    }
+}
