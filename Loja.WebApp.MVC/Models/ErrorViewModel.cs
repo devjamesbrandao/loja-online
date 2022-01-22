07688d4e6bcbc0424dc1bf1477d3e60dfa66a73b
James Brandao <muricijames@gmail.com>
2022-01-21 22:34:36 -0300

    Remoção de nullable

--- Loja.WebApp.MVC/Models/ErrorViewModel.cs
+++ Loja.WebApp.MVC/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Loja.WebApp.MVC.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
