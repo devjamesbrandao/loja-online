d6f01bb9a1c89a1003b4f92b69d2629899514cde
James Brandao <muricijames@gmail.com>
2022-01-26 22:25:51 -0300

    Status do pedido

---
+++ Loja.Venda.Dominio/Enum/PedidoStatus.cs
@@ -0,0 +1,11 @@
+namespace Loja.Venda.Dominio.Enum
+{
+    public enum PedidoStatus
+    {
+        Rascunho = 0,
+        Iniciado = 1,
+        Pago = 4,
+        Entregue = 5,
+        Cancelado = 6
+    }
+}
