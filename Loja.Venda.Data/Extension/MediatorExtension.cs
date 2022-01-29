ccbe061cd9552370006dce6a4f433a5611ad2fde
James Brandao <muricijames@gmail.com>
2022-01-28 23:14:46 -0300

    Remover e limpar eventos

---
+++ Loja.Venda.Data/Extension/MediatorExtension.cs
@@ -0,0 +1,30 @@
+using Loja.Core.Comunicacao;
+using Loja.Core.ObjetosDominio;
+using Loja.Venda.Data.Context;
+
+namespace Loja.Venda.Data.Extension
+{
+    public static class MediatorExtension
+    {
+        // public static async Task PublicarEventos(this IMediatorHandler mediator, VendasContext ctx)
+        // {
+        //     var domainEntities = ctx.ChangeTracker
+        //         .Entries<Entidade>()
+        //         .Where(x => x.Entity.Notificacoes != null && x.Entity.Notificacoes.Any());
+
+        //     var domainEvents = domainEntities
+        //         .SelectMany(x => x.Entity.Notificacoes)
+        //         .ToList();
+
+        //     domainEntities.ToList()
+        //         .ForEach(entity => entity.Entity.LimparEventos());
+
+        //     var tasks = domainEvents
+        //         .Select(async (domainEvent) => {
+        //             await mediator.PublicarEvento(domainEvent);
+        //         });
+
+        //     await Task.WhenAll(tasks);
+        // }
+    }
+}
