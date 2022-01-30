d91270683cc3b6b867c9ae5a6d3ec8e66aec85dd
James Brandao <muricijames@gmail.com>
2022-01-29 23:15:39 -0300

    View Models

---
+++ Loja.Venda.Aplicacao/Queries/ViewModels/CarrinhoItemViewModel.cs
@@ -0,0 +1,12 @@
+
+namespace Loja.Venda.Aplicacao.Queries.ViewModels
+{
+    public class CarrinhoItemViewModel
+    {
+        public Guid ProdutoId { get; set; }
+        public string ProdutoNome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal { get; set; }
+    }
+}
