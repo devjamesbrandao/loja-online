2f60fec0335894e2212a37dc6389e9f41b1dea0c
James Brandao <muricijames@gmail.com>
2022-01-29 21:51:56 -0300

    Componente para exibir erros na tela

---
+++ Loja.WebApp.MVC/Extensions/SummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Loja.Core.Message.Notificacoes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loja.WebApp.MVC.Extensions
+{
+    public class SummaryViewComponent : ViewComponent
+    {
+        private readonly NotificacaoDominioHandler _notifications;
+
+        public SummaryViewComponent(INotificationHandler<NotificacaoDominio> notifications)
+        {
+            _notifications = (NotificacaoDominioHandler)notifications;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notificacoes = await Task.FromResult(_notifications.ObterNotificacoes());
+
+            notificacoes.ForEach(c => ViewData.ModelState.AddModelError(string.Empty, c.Valor));
+
+            return View();
+        }
+    }
+}
