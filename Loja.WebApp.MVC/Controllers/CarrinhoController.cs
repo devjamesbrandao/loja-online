06ee354823f72b2d80b2bdab74953391b8722dd5
James Brandao <muricijames@gmail.com>
2022-01-29 21:51:24 -0300

    Controller carrinho; adicionar item no carrinho

---
+++ Loja.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -0,0 +1,57 @@
+using Loja.Catalogo.Aplicacao.Services;
+using Loja.Core.Comunicacao;
+using Loja.Core.Message.Notificacoes;
+using Loja.Vendas.Aplicacao.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loja.WebApp.MVC.Controllers
+{
+    public class CarrinhoController : ControllerBase
+    {
+        private readonly IProdutoAppService _produtoAppService;
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public CarrinhoController(
+            INotificationHandler<NotificacaoDominio> notifications,
+            IProdutoAppService produtoAppService, 
+            IMediatorHandler mediatorHandler
+        ) : base(notifications, mediatorHandler)
+        {
+            _produtoAppService = produtoAppService;
+            _mediatorHandler = mediatorHandler;
+        }
+
+
+        [HttpPost]
+        [Route("meu-carrinho")]
+        public async Task<IActionResult> AdicionarItem(Guid id, int quantidade)
+        {
+            var produto = await _produtoAppService.ObterPorId(id);
+
+            if (produto == null) return BadRequest();
+
+            if (produto.QuantidadeEstoque < quantidade)
+            {
+                TempData["Erro"] = "Produto com estoque insuficiente";
+
+                return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+            }
+
+            var command = new AdicionarItemPedidoCommand(
+                ClienteId, produto.Id, produto.Nome, quantidade, produto.Valor
+            );
+            
+            await _mediatorHandler.EnviarComando(command);
+
+            if (OperacaoValida())
+            {
+                return RedirectToAction("Index");
+            }
+
+            TempData["Erros"] = ObterMensagensErro();
+            
+            return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+        }
+    }
+}
