cc751da0a9c355ff60a5cb90c54a5350bca209c9
James Brandao <muricijames@gmail.com>
2022-02-03 22:12:48 -0300

    DTO de produtos

---
+++ Loja.Core/ObjetosDominio/DTO/ListaProdutosPedido.cs
@@ -0,0 +1,14 @@
+namespace Loja.Core.ObjetosDominio.DTO
+{
+    public class ListaProdutosPedido
+    {
+        public Guid PedidoId { get; set; }
+        public ICollection<Item> Itens { get; set; }
+    }
+
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
