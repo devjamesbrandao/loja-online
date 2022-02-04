bcbb4e57cf621dbdee5399a145dcb4b957e6270e
James Brandao <muricijames@gmail.com>
2022-02-03 22:12:12 -0300

    Evento para estoque rejeitado

---
+++ Loja.Core/Messages/MensagensComum/EventosDeIntegracao/PedidoEstoqueRejeitadoEvent.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Loja.Core.Message.MensagensComum.EventoDeIntegracao
+{
+    public class PedidoEstoqueRejeitadoEvent : EventoIntegracao
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+
+        public PedidoEstoqueRejeitadoEvent(Guid pedidoId, Guid clienteId)
+        {
+            IdAgregado = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+        }
+    }
+}
