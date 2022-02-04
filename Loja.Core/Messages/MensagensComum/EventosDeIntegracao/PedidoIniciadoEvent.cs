044d1cbdd9b8d01c79d5cf1167ad226e964736b4
James Brandao <muricijames@gmail.com>
2022-02-03 22:12:27 -0300

    Evento para pedido iniciado

---
+++ Loja.Core/Messages/MensagensComum/EventosDeIntegracao/PedidoIniciadoEvent.cs
@@ -0,0 +1,30 @@
+﻿
+using Loja.Core.ObjetosDominio.DTO;
+
+namespace Loja.Core.Message.MensagensComum.EventoDeIntegracao
+{
+    public class PedidoIniciadoEvent : EventoIntegracao
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public decimal Total { get; private set; }
+        public ListaProdutosPedido ProdutosPedido { get; private set; }
+        public string NomeCartao { get; private set; }
+        public string NumeroCartao { get; private set; }
+        public string ExpiracaoCartao { get; private set; }
+        public string CvvCartao { get; private set; }
+
+        public PedidoIniciadoEvent(Guid pedidoId, Guid clienteId, ListaProdutosPedido itens, decimal total, string nomeCartao, string numeroCartao, string expiracaoCartao, string cvvCartao)
+        {
+            IdAgregado = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+            ProdutosPedido = itens;
+            Total = total;
+            NomeCartao = nomeCartao;
+            NumeroCartao = numeroCartao;
+            ExpiracaoCartao = expiracaoCartao;
+            CvvCartao = cvvCartao;
+        }
+    }
+}
