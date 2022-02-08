c7fa433029d5f59401bd0471a7cf477213a38cdc
James Brandao <muricijames@gmail.com>
2022-02-07 21:56:30 -0300

    Entidade de pagamento

---
+++ Loja.Pagamento.Dominio/Entidades/Pagamento.cs
@@ -0,0 +1,20 @@
+﻿using Loja.Core.Interfaces;
+using Loja.Core.ObjetosDominio;
+
+namespace Loja.Pagamento.Dominio.Entidades
+{
+    public class Pagamento : Entidade, IAggregateRoot
+    {
+        public Guid PedidoId { get; set; }
+        public string Status { get; set; }
+        public decimal Valor { get; set; }
+
+        public string NomeCartao { get; set; }
+        public string NumeroCartao { get; set; }
+        public string ExpiracaoCartao { get; set; }
+        public string CvvCartao { get; set; }
+
+        // EF. Rel.
+        public Transacao Transacao { get; set; }
+    }
+}
