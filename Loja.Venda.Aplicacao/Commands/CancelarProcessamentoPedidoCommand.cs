227e95168e8a792ca1e7963583845e10fd148342
James Brandao <muricijames@gmail.com>
2022-02-08 20:49:26 -0300

    Comando cancelar pedido

---
+++ Loja.Venda.Aplicacao/Commands/CancelarProcessamentoPedidoCommand.cs
@@ -0,0 +1,17 @@
+﻿using Loja.Core.Message;
+
+namespace Loja.Venda.Aplicacao.Commands
+{
+    public class CancelarProcessamentoPedidoCommand : Comando
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+
+        public CancelarProcessamentoPedidoCommand(Guid pedidoId, Guid clienteId)
+        {
+            IdAgregado = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+        }
+    }
+}
