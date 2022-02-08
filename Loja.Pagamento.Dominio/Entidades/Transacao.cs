83054a11350c49f302623c0dcbdaa408743882a1
James Brandao <muricijames@gmail.com>
2022-02-07 21:56:59 -0300

    Entidade de transação

---
+++ Loja.Pagamento.Dominio/Entidades/Transacao.cs
@@ -0,0 +1,16 @@
+﻿
+using Loja.Core.ObjetosDominio;
+
+namespace Loja.Pagamento.Dominio.Entidades
+{
+    public class Transacao : Entidade
+    {
+        public Guid PedidoId { get; set; }
+        public Guid PagamentoId { get; set; }
+        public decimal Total { get; set; }
+        public StatusTransacao StatusTransacao { get; set; }
+
+        // EF. Rel.
+        public Pagamento Pagamento { get; set; }
+    }
+}
