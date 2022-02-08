7d6e3fdd12fcc7d6fb2c3a63c1da86d1b9dbe0d3
James Brandao <muricijames@gmail.com>
2022-02-08 20:49:12 -0300

    Cancelar pedido

---
+++ Loja.Core/Messages/MensagensComum/EventosDeIntegracao/PedidoProcessamentoCanceladoEvent.cs
@@ -0,0 +1,21 @@
+﻿
+using Loja.Core.Message.MensagensComum.EventoDeIntegracao;
+using Loja.Core.ObjetosDominio.DTO;
+
+namespace Loja.Core.Message.MensagensComum
+{
+    public class PedidoProcessamentoCanceladoEvent : EventoIntegracao
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public ListaProdutosPedido ProdutosPedido { get; private set; }
+
+        public PedidoProcessamentoCanceladoEvent(Guid pedidoId, Guid clienteId, ListaProdutosPedido produtosPedido)
+        {
+            IdAgregado = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+            ProdutosPedido = produtosPedido;
+        }
+    }
+}
