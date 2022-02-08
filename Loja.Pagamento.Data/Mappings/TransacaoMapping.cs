7c73b5d9d9fbe813488079e568e09d2f4c1cd409
James Brandao <muricijames@gmail.com>
2022-02-07 21:55:52 -0300

    Mapping das entidades

---
+++ Loja.Pagamento.Data/Mappings/TransacaoMapping.cs
@@ -0,0 +1,16 @@
+﻿using Loja.Pagamento.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Loja.Pagamentos.Data.Mappings
+{
+    public class TransacaoMapping : IEntityTypeConfiguration<Transacao>
+    {
+        public void Configure(EntityTypeBuilder<Transacao> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.ToTable("Transacoes");
+        }
+    }
+}
