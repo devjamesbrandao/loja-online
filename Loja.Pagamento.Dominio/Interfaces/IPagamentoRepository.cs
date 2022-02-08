12c26ef2925f2911e53b96ce247f33a9e6febed5
James Brandao <muricijames@gmail.com>
2022-02-07 21:57:42 -0300

    Interface do repositório

---
+++ Loja.Pagamento.Dominio/Interfaces/IPagamentoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Loja.Core.Data;
+
+namespace Loja.Pagamento.Dominio.Interfaces
+{
+    public interface IPagamentoRepository : IRepository<Loja.Pagamento.Dominio.Entidades.Pagamento>
+    {
+        void Adicionar(Loja.Pagamento.Dominio.Entidades.Pagamento pagamento);
+
+        void AdicionarTransacao(Loja.Pagamento.Dominio.Entidades.Transacao transacao);
+    }
+}
