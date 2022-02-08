b2a570dc9326d83e073ea1502c3002c6b1bb4177
James Brandao <muricijames@gmail.com>
2022-02-07 21:55:39 -0300

    Extensão do Mediator para publicar eventos

---
+++ Loja.Pagamento.Data/Extension/MediatorExtension.cs
@@ -0,0 +1,31 @@
+﻿
+using Loja.Core.Comunicacao;
+using Loja.Core.ObjetosDominio;
+using Loja.Pagamento.Data.Context;
+
+namespace Loja.Pagamento.Data.Extension
+{
+    public static class MediatorExtension
+    {
+        public static async Task PublicarEventos(this IMediatorHandler mediator, PagamentoContext ctx)
+        {
+            var domainEntities = ctx.ChangeTracker
+                .Entries<Entidade>()
+                .Where(x => x.Entity.Notificacoes != null && x.Entity.Notificacoes.Any());
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.Notificacoes)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.LimparEventos());
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) => {
+                    await mediator.PublicarEvento(domainEvent);
+                });
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
