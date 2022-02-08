c9eb6212e75ab00dae701c901ab3b7888ca6cee8
James Brandao <muricijames@gmail.com>
2022-02-08 20:49:45 -0300

    Estornar estoque

---
+++ Loja.Venda.Aplicacao/Commands/CancelarProcessamentoPedidoEstornarEstoqueCommand.cs
@@ -0,0 +1,18 @@
+﻿
+using Loja.Core.Message;
+
+namespace Loja.Venda.Aplicacao.Commands
+{
+    public class CancelarProcessamentoPedidoEstornarEstoqueCommand : Comando
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+
+        public CancelarProcessamentoPedidoEstornarEstoqueCommand(Guid pedidoId, Guid clienteId)
+        {
+            IdAgregado = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+        }
+    }
+}
