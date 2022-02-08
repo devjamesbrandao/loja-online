859c7614ddeb95c3a86417e68b003e49e49269bf
James Brandao <muricijames@gmail.com>
2022-02-07 21:57:20 -0300

    Handler do pedido

---
+++ Loja.Pagamento.Dominio/Eventos/PagamentoEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using Loja.Core.Message.MensagensComum.EventoDeIntegracao;
+using Loja.Core.ObjetosDominio.DTO;
+using Loja.Pagamento.Dominio.Interfaces;
+using MediatR;
+
+namespace Loja.Pagamento.Dominio.Eventos
+{
+    public class PagamentoEventHandler : INotificationHandler<PedidoEstoqueConfirmadoEvent>
+    {
+        private readonly IPagamentoService _pagamentoService;
+
+        public PagamentoEventHandler(IPagamentoService pagamentoService)
+        {
+            _pagamentoService = pagamentoService;
+        }
+
+        public async Task Handle(PedidoEstoqueConfirmadoEvent message, CancellationToken cancellationToken)
+        {
+            var pagamentoPedido = new PagamentoPedido
+            {
+                PedidoId = message.PedidoId,
+                ClienteId = message.ClienteId,
+                Total = message.Total,
+                NomeCartao = message.NomeCartao,
+                NumeroCartao = message.NumeroCartao,
+                ExpiracaoCartao = message.ExpiracaoCartao,
+                CvvCartao = message.CvvCartao
+            };
+
+            await _pagamentoService.RealizarPagamentoPedido(pagamentoPedido);
+        }
+    }
+}
