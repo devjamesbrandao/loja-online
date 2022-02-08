d72ce38d432df5d074d7d140230e07d1c9230b93
James Brandao <muricijames@gmail.com>
2022-02-07 21:56:08 -0300

    Repositório de pedidos

---
+++ Loja.Pagamento.Data/Repository/PedidoRepository.cs
@@ -0,0 +1,36 @@
+﻿
+using Loja.Core.Data;
+using Loja.Pagamento.Data.Context;
+using Loja.Pagamento.Dominio.Entidades;
+using Loja.Pagamento.Dominio.Interfaces;
+
+namespace Loja.Pagamento.Data.Repository
+{
+    public class PagamentoRepository : IPagamentoRepository
+    {
+        private readonly PagamentoContext _context;
+
+        public PagamentoRepository(PagamentoContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+
+        public void Adicionar(Loja.Pagamento.Dominio.Entidades.Pagamento pagamento)
+        {
+            _context.Pagamentos.Add(pagamento);
+        }
+
+        public void AdicionarTransacao(Transacao transacao)
+        {
+            _context.Transacoes.Add(transacao);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
