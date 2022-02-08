eb732ef8092f89795d6674e80256c7ff2c9a2c1b
James Brandao <muricijames@gmail.com>
2022-02-08 20:49:54 -0300

    Finalizar pedido

---
+++ Loja.Venda.Aplicacao/Commands/FinalizarPedidoCommand.cs
@@ -0,0 +1,17 @@
+﻿using Loja.Core.Message;
+
+namespace Loja.Venda.Aplicacao.Commands
+{
+    public class FinalizarPedidoCommand : Comando
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+
+        public FinalizarPedidoCommand(Guid pedidoId, Guid clienteId)
+        {
+            IdAgregado = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+        }
+    }
+}
