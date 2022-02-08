ca64cbb923a1a7bfdd0e4af562bd7b7affec95c8
James Brandao <muricijames@gmail.com>
2022-02-07 21:57:33 -0300

    Interface da facade de pagamento

---
+++ Loja.Pagamento.Dominio/Interfaces/IPagamentoCartaoCreditoFacade.cs
@@ -0,0 +1,9 @@
+﻿using Loja.Pagamento.Dominio.Entidades;
+
+namespace Loja.Pagamento.Dominio.Interfaces
+{
+    public interface IPagamentoCartaoCreditoFacade
+    {
+        Transacao RealizarPagamento(Pedido pedido, Loja.Pagamento.Dominio.Entidades.Pagamento pagamento);
+    }
+}
