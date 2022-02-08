15cac1b821edd3bafc561d3109808c02237c093b
James Brandao <muricijames@gmail.com>
2022-02-08 20:50:10 -0300

    Evento de pedido finalizado

---
+++ Loja.Venda.Aplicacao/Events/PedidoFinalizadoEvent.cs
@@ -0,0 +1,16 @@
+﻿
+using Loja.Core.Message;
+
+namespace Loja.Venda.Aplicacao.Events
+{
+    public class PedidoFinalizadoEvent : Evento
+    {
+        public Guid PedidoId { get; private set; }
+
+        public PedidoFinalizadoEvent(Guid pedidoId)
+        {
+            PedidoId = pedidoId;
+            IdAgregado = pedidoId;
+        }
+    }
+}
