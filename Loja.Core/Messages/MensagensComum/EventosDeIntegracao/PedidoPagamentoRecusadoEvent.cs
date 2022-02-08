2df7fa75d821e049ae977c50490cd7688fb83bba
James Brandao <muricijames@gmail.com>
2022-02-07 21:53:35 -0300

    Evento pedido recusado

---
+++ Loja.Core/Messages/MensagensComum/EventosDeIntegracao/PedidoPagamentoRecusadoEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace Loja.Core.Message.MensagensComum.EventoDeIntegracao
+{
+    public class PedidoPagamentoRecusadoEvent : EventoIntegracao
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Guid PagamentoId { get; private set; }
+        public Guid TransacaoId { get; private set; }
+        public decimal Total { get; private set; }
+
+        public PedidoPagamentoRecusadoEvent(Guid pedidoId, Guid clienteId, Guid pagamentoId, Guid transacaoId, decimal total)
+        {
+            IdAgregado = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+            PagamentoId = pagamentoId;
+            TransacaoId = transacaoId;
+            Total = total;
+        }
+    }
+}
